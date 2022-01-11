0c0ebbdf012472f63d50853e8cefe09b019ced04
Vegito <codingemail05@gmail.com>
2022-01-12 01:37:17 +0530

    Small Screen Complete

--- app.js
+++ app.js
@@ -4,3 +4,23 @@ const navUl = document.getElementById("nav-ul")
 hamburger.addEventListener("click", () => {
     navUl.classList.toggle("show");
 })
+
+const python = document.getElementById("python")
+const list = document.getElementById("list")
+
+
+python.addEventListener("click", () => {
+    list.classList.toggle("show-lists")
+    skill.classList.toggle("show-skill")
+})
+
+const web = document.getElementById("web")
+const list_2 = document.getElementById("list-2")
+const skill = document.getElementById("skills")
+
+web.addEventListener("click", () => {
+    list_2.classList.toggle("show-lists");
+    skill.classList.toggle("show-skill")
+
+})
+
